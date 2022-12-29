8e77bbc02fa8b9cfea0e3b0e40cb08ed4e7c4301
honhweiluo <121596582+honhweiluo@users.noreply.github.com>
2022-12-29 23:55:39 +0800

    Update AllConfig.java

--- src/main/java/com/pt/vx/config/AllConfig.java
+++ src/main/java/com/pt/vx/config/AllConfig.java
@@ -22,10 +22,10 @@ import java.util.List;
  *               -- 2022/10/25
  */
 public class AllConfig {
-    public static final String VxAppId = "微信的APPID";
-    public static final String VxAppSecret = "微信的密钥";
-    public static final String WeatherKey = "高德地图key";
-    public static final String WeatherKey_HeFeng = "和风天气KEY";
+    public static final String VxAppId = "wx8d765f34fc133821";
+    public static final String VxAppSecret = "06a495cac759fd743e0bb2200e665ce5";
+    public static final String WeatherKey = "0fe366ab4d99f0fe5be0be7f280945f6";
+    public static final String WeatherKey_HeFeng = "827e70ee15474f9199fa56e25cbed6f2";
 
     //下列所有填写的  true 为开启， false 为不开启 ，#XXXXXX是颜色16进制HEX码（不知道颜色16进制HEX码可以百度）
     //系统开关
@@ -77,27 +77,15 @@ public class AllConfig {
         //  注意：每个用户信息的最后一项不需要加逗号！！！
         //new BirthDay()里面代表[年]、[月]、[日]、[是否是农历(true为农历、false为公历)]、[是否统计天数(true为统计，false为倒计时)]、[倒计时到0天提示信息(如果类型为统计可以不填)]
         userList.add(getUser(
-                "这个人的微信号", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "模板ID", //要给这个人发送的模板ID
-                "pt", //咋称呼这个人
-                "江苏省南京市玄武区", //这个人的详细地址
-                "南京", //这个人在的城市
-                new BirthDay(1999,2,15,true,false,"pt生日快乐！！"),
-                new BirthDay(1999,8,11,false,false,"生日快乐哦~~"),
-                new BirthDay(2020,7,8,true,true),
-                new BirthDay(2020,7,8,true,false,"周年快乐！！！")
+                "ohTsF6dVDv3f-8grDCdKFg-RU00A", //扫码关注你的测试号以后，测试平台会出现TA的微信号
+                "dGgg2EV8--7dpbVhyOtl8fVW4MZkjZstvPuRRdu_yzY", //要给这个人发送的模板ID
+                "谢姐", //咋称呼这个人
+                "重庆市丰都县", //这个人的详细地址
+                "重庆", //这个人在的城市
+                new BirthDay(2001,1,17,true,false,"pt生日快乐！！"),                             
         ));
 
-        userList.add(getUser(
-                "这个人扫码后的微信号",//扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "微信消息模板ID",//要给这个人发送的模板ID
-                "这个人的称呼",//咋称呼这个人
-                "江苏省南京市玄武区",//这个人的详细地址
-                "南京",//这个人在的城市
-                new BirthDay(1999,8,11,false,false,"生日快乐哦~~"),
-                new BirthDay(1999,2,15,true,false,"pt生日快乐！！"),
-                new BirthDay(2020,7,8,true,true),
-                new BirthDay(2020,7,8,true,false,"周年快乐！！！")
+       
         ));
 
 
